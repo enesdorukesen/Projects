97f2c8d655c4dbf19dc7e2d67ca4f8377f7252b2
duffgor <fesn34@gmail.com>
2022-06-11 00:57:35 +0300

    initial

---
+++ reducer2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReducerExample from "./ReducerExample";
+import UseStateExample from "./UseStateExample";
+
+function App() {
+  return (
+    <div>
+      {/* <UseStateExample /> */}
+      <ReducerExample />
+    </div>
+  );
+}
+
+export default App;
