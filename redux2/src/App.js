e1730ee84a8457fd41403f87e3aa8284a028583f
duffgor <fesn34@gmail.com>
2022-06-11 18:09:07 +0300

    initial

--- redux2/src/App.js
+++ redux2/src/App.js
@@ -1,5 +1,58 @@
-function App() {
-  return <div className="App">EDE</div>;
+import React from "react";
+import AppRouter from "./router";
+import { legacy_createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider as StoreProvider } from "react-redux";
+
+export const initialstate = {
+  loading: false,
+  token: "",
+  error: [],
+  language: "en",
+  userList: [],
+  selectedUser: {},
+  newsList: [],
+  selectedNews: {},
+};
+
+export const rootReducer = (state = initialstate, { type, payload }) => {
+  switch (type) {
+    case "SET_LOADING_FALSE":
+      return { ...state, loading: false };
+    case "SET_LOADING_TRUE":
+      return { ...state, loading: true };
+    case "SET_TOKEN":
+      return { ...state, token: payload };
+    case "SET_ERROR":
+      return { ...state, error: payload };
+    case "SET_LANGUAGE":
+      return { ...state, language: payload };
+    case "CLEAR_ERROR":
+      return { ...state, error: [] };
+    default:
+      return state;
+  }
+};
+
+let store;
+if (process.env.NODE_ENV === "development") {
+  store = legacy_createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 }
 
+const App = () => {
+  return (
+    <StoreProvider store={store}>
+      <AppRouter />
+    </StoreProvider>
+  );
+};
 export default App;
