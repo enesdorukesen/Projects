9a5989847a7ac49957f84d6ccea98bcb1075725e
duffgor <fesn34@gmail.com>
2022-06-11 22:07:28 +0300

    initial

--- redux2/src/App.js
+++ redux2/src/App.js
@@ -1,63 +1,27 @@
 import React from "react";
 import AppRouter from "./router";
-import { legacy_createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import store from "./redux/store";
 import { Provider as StoreProvider } from "react-redux";
 
-export const initialstate = {
-  loading: false,
-  token: "",
-  error: [],
-  language: "en",
-  userList: [],
-  selectedUser: {},
-  newsList: [],
-  selectedNews: {},
-};
+// export const initialstate = {
+//   newsList: [],
+//   selectedNews: {},
+// };
 
-export const rootReducer = (state = initialstate, { type, payload }) => {
-  switch (type) {
-    case "SET_LOADING_FALSE":
-      return { ...state, loading: false };
-    case "SET_LOADING_TRUE":
-      return { ...state, loading: true };
-    case "SET_TOKEN":
-      return { ...state, token: payload };
-    case "SET_ERROR":
-      return { ...state, error: payload };
-    case "SET_LANGUAGE":
-      return { ...state, language: payload };
-    case "CLEAR_ERROR":
-      return { ...state, error: [] };
-    case "SET_USER_LIST":
-      return { ...state, userList: payload };
-    case "SET_SELECTED_USER":
-      return { ...state, selectedUser: payload };
-    case "SET_NEWS_LIST":
-      return { ...state, newsList: payload };
-    case "SET_SELECTED_NEWS":
-      return { ...state, selectedNews: payload };
-    default:
-      return state;
-  }
-};
-
-let store;
-if (process.env.NODE_ENV === "development") {
-  store = legacy_createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = legacy_createStore(rootReducer, applyMiddleware(thunk));
-}
-store.subscribe(() => {
-  store.getState();
-});
+// export const rootReducer = (state = initialstate, { type, payload }) => {
+//   switch (type) {
+//     case "SET_USER_LIST":
+//       return { ...state, userList: payload };
+//     case "SET_SELECTED_USER":
+//       return { ...state, selectedUser: payload };
+//     case "SET_NEWS_LIST":
+//       return { ...state, newsList: payload };
+//     case "SET_SELECTED_NEWS":
+//       return { ...state, selectedNews: payload };
+//     default:
+//       return state;
+//   }
+// };
 
 const App = () => {
   return (
