f8bfd215fff8bc6a64af1e81e953a37657c49db6
duffgor <fesn34@gmail.com>
2022-08-12 10:07:16 +0300

    initial

---
+++ apps/blog/src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import { analytics } from "./fireBase";
+
+console.log(analytics);
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ apps/youtube/storefront/playgorund/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PlaygorundConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'playgorund'
